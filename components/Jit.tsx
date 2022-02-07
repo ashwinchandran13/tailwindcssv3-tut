e6c520030e228039835647d78a291a78899400b7
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-02-07 22:30:00 +0530

    added Jit slide

---
+++ components/Jit.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Jit = () => {
+    return (
+        <div className='bg-gradient-to-br from-blue-300 to-cyan-100 h-screen w-screen flex justify-start'>
+            <ul className='font-poppins font-medium list-disc self-center mx-44 text-5xl leading-relaxed tracking-normal'>
+                <li>Huge Performance Boost</li>
+                <li>Arbitary Values😎</li>
+                <li>All Variants Enabled/VariantsStackable Variants</li>
+                <li>No CSS Purge Anymore!</li>
+                <li>Better Browser Performance</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Jit;
