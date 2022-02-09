e0e1e975dfb0cce04cb02e63e445e47efc92f2c2
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-02-09 23:12:03 +0530

    added scroll data

---
+++ components/ScrollSnap.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ScrollSnap = () => {
+  return (
+      <div>
+          <li>
+              <div className="relative flex-shrink-0 max-w-"></div>
+          </li>
+      </div>
+  );
+};
+
+export default ScrollSnap;
---
+++ constants/data.ts
@@ -0,0 +1,40 @@
+
+
+export const scrollData = [
+    {
+        id: 1,
+        name: 'Enterprise Architecture',
+        title: 'Complex Yet High Performance',
+        image_path: 'https://studioghiblimovies.com/wp-content/uploads/2020/03/p08192c7.jpg'
+    },
+    {
+        id: 2,
+        name: 'System Programming',
+        title: 'Updated to Current Times',
+        image_path: 'https://i.pinimg.com/originals/51/81/a3/5181a33c0955dee17b6e5752c09b9a3c.jpg'
+    },
+    {
+        id: 3,
+        name: 'Web & Mobile',
+        title: 'Fluid Experience Everywhere',
+        image_path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4LajvhHzSGjZ3sUDyUYU6I9oWGTkGxXHhpA&usqp=CAU'
+    },
+    {
+        id: 4,
+        name: 'Machine Learning',
+        title: 'Growing Neurals Within',
+        image_path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX83VrR5pxxMMrO3NiX5gG4Ia1xk0KNTyIeQ&usqp=CAU'
+    },
+    {
+        id: 5,
+        name: '3D Rendering',
+        title: 'Visualization is the Way',
+        image_path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3iHbYNuiLpJGVJNsIcTgNoC_vBbtmG-c9spJPERFo3FvBWWeeR0mmr3jtAAynexMNkfM&usqp=CAU'
+    },
+    {
+        id: 6,
+        name: 'Data Analytics',
+        title: 'Understanding Data Game',
+        image_path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTveb5LCdFxIwZ-Ia_fWdVcmcPp_i79rMRa6A&usqp=CAU'
+    },
+]
