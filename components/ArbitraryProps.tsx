40599f98afdb981ac488e86eb84c4683682223fc
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-02-13 21:27:56 +0530

    finalization

---
+++ components/ArbitraryProps.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ArbitraryProps = () => {
+  return (
+    <div className='grid max-w-5xl min-h-screen mx-auto mt-5 text-gray-500 place-items-center'>
+        <div className='flex'>
+        {/** add arbitary value like [clip-path:circle(70%_at_20%_30%)] */}
+        <img 
+        src="https://o.aolcdn.com/images/dar/5845cadfecd996e0372f/4b9f2b75c4a3e68808d181306457d6fcb40966e7/aHR0cDovL3d3dy5ibG9nY2RuLmNvbS93d3cuam95c3RpcS5jb20vbWVkaWEvMjAxMy8wNS9iYW9rYWZpbmFsLmpwZw==" 
+        alt="batman-pic" 
+        className='float-left object-cover object-center w-64 mb-0 mr-6 rounded-lg shadow-lg aspect-square'
+        />
+        <p className='text-3xl leading-relaxed tracking-wide'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error vel perspiciatis consectetur itaque reprehenderit? Esse numquam velit vero dolor similique odio adipisci magni, alias culpa. Cupiditate impedit facere deleniti iste magni voluptate in. Atque iure beatae, pariatur recusandae ex ipsa doloribus fugiat dolor magni? Doloremque provident perspiciatis sit ipsam itaque?
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default ArbitraryProps
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import AccentColor from '../components/AccentColor';
+import ArbitraryProps from '../components/ArbitraryProps';
 import ColoredBox from '../components/ColoredBox';
 import Jit from '../components/Jit';
 import MultiColumn from '../components/MultiColumn';
@@ -12,73 +13,70 @@ import UnderlineStyles from '../components/UnderlineStyles';
 const index = () => {
   return (
     <div className='relative overflow-x-clip'>
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>tailwindcss V3.0</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>What's new?</h5>
       </div>
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Just-In-Time Engine</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>Lighting Fast Build Times⚡</h5>
       </div>
       <Jit />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>More Colors Palletes</h1>
         <a href='https://tailwindcss.com/docs/customizing-colors' className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-lime-500 hover:scale-110'>Color Reference🌈</a>
       </div>
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Colored Box Shadows</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>Glow and Reflections💫</h5>
       </div>
       <ColoredBox />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Print Modifier</h1>
         <Link href='/print-modifier'><a className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400 hover:scale-110'>CSS for Printing🖨️</a></Link>
       </div>
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Native Aspect Ratios</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>Without plugin🌝</h5>
       </div>
       <NativeAspect />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Comprehensive Scroll Snap</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>CSS only💪</h5>
       </div>
       <ScrollSnap />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Scroll Behaviour</h1>
         <Link href='/scroll-behaviour'><a className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400 hover:scale-110'>Scroll to right place⬇️</a></Link>
       </div>
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Multi-Column Layout</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>Newspaper for web📰</h5>
       </div>
       <MultiColumn />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Accent Color & File Inputs</h1>
         <h5 className='self-center mt-3 text-4xl text-white underline shadow-lg decoration-teal-400'>Native form on brand</h5>
       </div>
       <AccentColor />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Open Modifier</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>Styling open/closed states⏬</h5>
       </div>
       <OpenModifier />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
         <h1 className='self-center text-white text-8xl mx-44 '>Fancy Underline Styles</h1>
         <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>Links deserve better🌼</h5>
       </div>
       <UnderlineStyles />
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
-        <h1 className='self-center text-white text-8xl mx-44 '>tailwindcss V3.0</h1>
-        <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>What's new?</h5>
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
+        <h1 className='self-center text-white text-8xl mx-44 '>Arbitrary Properties</h1>
+        <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>In depth⛏️</h5>
       </div>
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
-        <h1 className='self-center text-white text-8xl mx-44 '>tailwindcss V3.0</h1>
-        <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>What's new?</h5>
-      </div>
-      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-indigo-900 to-cyan-800'>
-        <h1 className='self-center text-white text-8xl mx-44 '>tailwindcss V3.0</h1>
-        <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>What's new?</h5>
+      <ArbitraryProps />
+      <div className='flex justify-start w-screen h-screen bg-gradient-to-br from-sky-400 to-cyan-800'>
+        <h1 className='self-center text-white text-8xl mx-44 '>Play CDN</h1>
+        <h5 className='self-center mt-3 text-5xl text-white underline shadow-lg decoration-teal-400'>Just-In-Time, in the browser😮</h5>
       </div>
     </div>
   );
